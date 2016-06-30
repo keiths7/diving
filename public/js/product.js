8eaa0d0720a5815fdb817ebfb72c1ec37a84af5d
唐程 <tangcheng@le.com>
2016-06-30 19:11:43 +0800

    add  product page first

---
+++ public/js/product.js
@@ -0,0 +1,6 @@
+
+$('.ui.rating').rating();
+
+$('.description.p70.column').on('click','.more',function(){
+    $(this).prev().toggleClass('expand')
+})
---
+++ server.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var app =express();
+var bodyParser = require('body-parser');
+var path = require('path');
+app.set('port', (process.env.PORT || 3300));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
