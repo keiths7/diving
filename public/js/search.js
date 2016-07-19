3d5dcd0e0e73f0b430cd84e400a74d781b16912b
tach0932 <tangcheng@le.com>
2016-07-19 19:59:23 +0800

    search page update content and js

--- public/js/search.js
+++ public/js/search.js
@@ -1,2 +1,12 @@
-$('.ui.checkbox')
-  .checkbox()
+$('.ui.checkbox').checkbox()
+var conditionDV=$('.s-condition');
+$('.filter-buttons .filter').on('click','button',function(){
+  conditionDV.css('height','618px');
+  conditionDV.addClass('open');
+})
+$('.apply-buttons').on('click','.ok,.cancel',function(){
+  conditionDV.removeClass('open');
+  $('body').animate({scrollTop:0},300,function(){
+    conditionDV.css('height','264px');
+  })
+})
