b0e8948af449c3b1d42eab287e0831a6a5946f2d
liushunqi <keithsliu@gmail.com>
2016-11-24 21:09:21 +0800

    test pay and bugfix

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -300,6 +300,7 @@
           </div>
         </div>
       </div>
+      @if(count($v) == 3)
       <div class="see-more-button">
         @if(isset($v[0]['city_info']))
         <div class="ui center aligned container "><button class="ui basic button" position="" city="{{ $v[0]['city_info']['id'] }}">    See More Locate Dive Sites    </button></div>
@@ -307,6 +308,7 @@
         <div class="ui center aligned container "><button class="ui basic button" position="{{ $v[0]['id'] }}" city="">    See More Locate Dive Sites    </button></div>
         @endif
       </div>
+      @endif
       <div class="ui divider"> </div>
     </div>
     @endforeach
@@ -379,96 +381,6 @@
       <div class="ui divider"> </div>
     </div> -->
 </section>
-<!-- footer -->
-<footer class="footer-sec ui black inverted vertical footer segment">
-  <div class="ui center aligned container">
-    <div class="ui stackable inverted centered four column grid">
-      <div class="row">
-      <div class="column">
-        <h4 class="ui inverted header">Dream Scubatrip</h4>
-        <div class="ui inverted bolded divider "></div>
-        <div class="ui inverted link list">
-          <a class="item" href="https://www.transifex.com/organization/semantic-org/" target="_blank">
-            <i class="marker icon"></i>
-            BEIJING
-          </a>
-          <div class="ui inverted divider"></div>
-          <a class="item" href="https://github.com/Semantic-Org/Semantic-UI/issues" target="_blank">
-            <i class="phone icon"></i>
-            +86 18911189901
-          </a>
-          <div class="ui inverted divider"></div>
-          <a class="item" href="https://gitter.im/Semantic-Org/Semantic-UI" target="_blank">
-            <i class="mail outline icon"></i>
-            market@dreamdivingtrip.com
-          </a>
-        </div>
-      </div>
-      <div class="column">
-        <h4 class="ui inverted header">TAG</h4>
-        <div class="ui inverted bolded divider"></div>
-        <div class="ui   tiny labels">
-          <div class="ui inverted  label">BIG ANIMAL</div>
-          <div class="ui inverted label">STORE</div>
-          <div class="ui inverted label">HO</div>
-          <div class="ui inverted label">WALL</div>
-          <div class="ui inverted label">GO DIVING</div>
-          <div class="ui inverted label">SHADOW</div>
-          <div class="ui inverted label">FREEEEE</div>
-        </div>
-      </div>
-      <div class="column">
-        <h4 class="ui inverted header">GALLERY</h4>
-        <div class="ui inverted bolded divider"></div>
-        <!-- 底部的图片 -->
-        <div class="ui link three cards">
-            <div class="card">
-              <div class="image"><img src="/images/index/gallery.png" alt=""></div>
-            </div>
-            <div class="card">
-              <div class="image"><img src="/images/index/gallery.png" alt=""></div>
-            </div>
-            <div class="card">
-              <div class="image"><img src="/images/index/gallery.png" alt=""></div>
-            </div>
-            <div class="card">
-              <div class="image"><img src="/images/index/gallery.png" alt=""></div>
-            </div>
-            <div class="card">
-              <div class="image"><img src="/images/index/gallery.png" alt=""></div>
-            </div>
-        </div>
-      </div>
-      <div class="column">
-        <h4 class="ui inverted header">CONTACT US</h4>
-        <div class="ui inverted bolded divider"></div>
-        <form class="ui form" >
-          <div class=" ui field  input ">
-            <input type="text" name="email" placeholder="email">
-          </div>
-          <div class=" ui field  input">
-            <input type="text" name="message" placeholder="message">
-          </div>
-          <button class="ui mini teal button" type="submit">SEND</button>
-        </form>
-      </div>
-      </div>
-    </div>
-    <div class="ui  hidden  divider"></div>
-    <div class="ui two column grid">
-      <div class="seven wide left floated column">
-          <div class="ui inverted link list">
-            <a href="#" class="item">COPY RIGHT 2016 BY DREAMSCUBATRIP</a>
-          </div>
-      </div>
-      <div class="three wide right floated column">
-         <i class="bordered inverted  teal  twitter  icon"></i>&nbsp;
-         <i class="bordered inverted  red  google plus  icon"></i>&nbsp;
-         <i class="bordered inverted  blue facebook  icon"></i>
-      </div>
-    </div>
-  </div>
-</footer>
 @include('layouts.footer')
 @include('layouts.loginer')
 
