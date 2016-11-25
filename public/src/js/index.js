c697c8f58468a599be589cdb24730e6d95b66f41
tach0932 <tangcheng@le.com>
2016-11-25 21:03:24 +0800

    'fixloginbug'

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,129 +1,133 @@
-if(window.screen&&screen.width){
-   window.screenSize=screen.width>767?'PC':'MOBILE';
-}else{
-  window.screenSize='PC';
+if (window.screen && screen.width) {
+    window.screenSize = screen.width > 767 ? 'PC' : 'MOBILE';
+} else {
+    window.screenSize = 'PC';
 }
-var signUpDialog='',signInDialog='';
-$(window).scroll(function(){
-   if(window.screenSize=="MOBILE") return;
-   if(document.body.scrollTop>30){
+var signUpDialog = '',
+    signInDialog = '';
+$(window).scroll(function () {
+    if (window.screenSize == "MOBILE") return;
+    if (document.body.scrollTop > 30) {
         $('.following.bar').addClass('light');
         $('.menu').removeClass('inverted');
-   }else{
-       $('.following.bar').removeClass('light');
-       $('.menu').addClass('inverted');
-   }
+    } else {
+        $('.following.bar').removeClass('light');
+        $('.menu').addClass('inverted');
+    }
 })
 
-$('.search-bar').on('click','.search-btn',function(){
-      var text='';
-      if(window.screenSize=="PC"){
-        text=$(this).siblings('.four.wide').find('input[name=country]').val();
-      }else{
-        text=$(this).siblings('.ten.wide').find('input[name=country]').val();
-      }
-       search(text);
-      if(text){
-          ga('send', 'event', 'search_click', 'content:'+text, '');
-      } else{
-           ga('send', 'event', 'search_click', 'content:'+'null', '');
-      }
+$('.search-bar').on('click', '.search-btn', function () {
+    var text = '';
+    if (window.screenSize == "PC") {
+        text = $(this).siblings('.four.wide').find('input[name=country]').val();
+    } else {
+        text = $(this).siblings('.ten.wide').find('input[name=country]').val();
+    }
+    search(text);
+    if (text) {
+        ga('send', 'event', 'search_click', 'content:' + text, '');
+    } else {
+        ga('send', 'event', 'search_click', 'content:' + 'null', '');
+    }
 
 })
-$('.second-sec').on('click','.ui.list .item',function(){
-    var title=$(this).parent().siblings('.ui.header').text();
-    var linkText=$(this).text();
-    ga('send', 'event', 'course_click', 'title:'+title+',content:'+linkText, '');
+$('.second-sec').on('click', '.ui.list .item', function () {
+    var title = $(this).parent().siblings('.ui.header').text();
+    var linkText = $(this).text();
+    ga('send', 'event', 'course_click', 'title:' + title + ',content:' + linkText, '');
 })
-$('.third-sec').on('click','.card',function(){
-    var hotName=$(this).find('.content .ui.header').text();
-    ga('send', 'event', 'hotSpot_click', 'hotSpot_name:'+hotName, '');
+$('.third-sec').on('click', '.card', function () {
+    var hotName = $(this).find('.content .ui.header').text();
+    ga('send', 'event', 'hotSpot_click', 'hotSpot_name:' + hotName, '');
 })
-$('.fourth-sec').on('click','.ui.image',function(){
-    var popularName=$(this).find('.content .ui.header').text();
-    ga('send', 'event', 'popularSpot_click', 'popularSpot_name:'+popularName, '');
+$('.fourth-sec').on('click', '.ui.image', function () {
+    var popularName = $(this).find('.content .ui.header').text();
+    ga('send', 'event', 'popularSpot_click', 'popularSpot_name:' + popularName, '');
 })
-$('.more-course').on('click',function(){
+$('.more-course').on('click', function () {
     ga('send', 'event', 'seeMoreCourse_click', '', '');
 })
-$('.more-destination').on('click',function(){
+$('.more-destination').on('click', function () {
     ga('send', 'event', 'seeMoreDestination_click', '', '');
 })
-$('.ui.mini.teal.button').on('click',function(e){
-  ga('send', 'event', 'contant_us_click', '', '');
-  e.preventDefault();
+$('.ui.mini.teal.button').on('click', function (e) {
+    ga('send', 'event', 'contant_us_click', '', '');
+    e.preventDefault();
 })
 
-$(function(){
-  var destDOM= $('.ui.dropdown.destination');
-      signUpDialog=$('.ui.modal.signup-dialog');
-      signInDialog=$('.ui.modal.login-dialog');
-    destDOM.dropdown({  
-      direction: 'updown',
-      transition:'slide down',
-      allowAdditions:false,
-      // action:function(){      
-      // },
-      // onChange:function(value, text, selectedItem){
-      // },
-      // apiSettings: {
-      //    url: 'data.json?word={query}'
-      // }
+$(function () {
+        var destDOM = $('.ui.dropdown.destination');
+        signUpDialog = $('.ui.modal.signup-dialog');
+        signInDialog = $('.ui.modal.login-dialog');
+        destDOM.dropdown({
+            direction: 'updown',
+            transition: 'slide down',
+            allowAdditions: false,
+            // action:function(){      
+            // },
+            // onChange:function(value, text, selectedItem){
+            // },
+            // apiSettings: {
+            //    url: 'data.json?word={query}'
+            // }
+        })
+        $('.ui.dropdown.passengers').dropdown()
+        $('.date-pick').kuiDate({
+            className: 'date-pick',
+            isDisabled: "0" // isDisabled为可选参数，“0”表示今日之前不可选，“1”标志今日之前可选
+        });
+        $('.search-bar input.search').on('input propertychange', function () {
+            destDOM.addClass('loading');
+            var _this = $(this),
+                word = _this.val(),
+                itemStr = '';
+            $.getJSON('/destination', {
+                word: word
+            }, function (r) {
+                destDOM.removeClass('loading');
+                if ((r instanceof Array) && r.length > 0) {
+                    $(r).each(function (k, v) {
+                        itemStr += '<div class="item" data-value="' + v + '">' + v + '</div>';
+                    })
+                    _this.siblings('.menu').html(itemStr);
+                    destDOM.dropdown('refresh');
+                }
+            })
+        })
+        $('.user.ui.dropdown').dropdown({
+            on: 'hover',
+            action: function () {},
+        });
+
     })
-  $('.ui.dropdown.passengers').dropdown()
-  $('.date-pick').kuiDate({
-    className:'date-pick',
-    isDisabled: "0"  // isDisabled为可选参数，“0”表示今日之前不可选，“1”标志今日之前可选
-  });
-  $('.search-bar input.search').on('input propertychange',function(){
-     destDOM.addClass('loading');
-      var _this=$(this),word=_this.val(),itemStr='';
-      $.getJSON('/destination',{word:word},function(r){
-          destDOM.removeClass('loading');
-          if((r instanceof Array) && r.length>0){
-              $(r).each(function(k,v){
-                itemStr+='<div class="item" data-value="'+v+'">'+v+'</div>';
-              })
-              _this.siblings('.menu').html(itemStr);
-              destDOM.dropdown('refresh');
-          }          
-      })
-  })
-  $('.user.ui.dropdown').dropdown({
-    on:'hover',
-    action:function(){    
-    },
-  });
- 
-})
-//signin,signup
-$('.ui.modal').on('click','.signin-button',signIn)
-$('.ui.modal').on('click','.signup-button',signUp);
-$('header.ui.bar').on('click','.signin.item',signInAlert);
-$('header.ui.bar').on('click','.signup.item',signUpAlert);
-$('.more-destination').on('click','.ui.button',seeMoreDestination);
+    //signin,signup
+$('.ui.modal').on('click', '.signin-button', signIn)
+$('.ui.modal').on('click', '.signup-button', signUp);
+$('header.ui.bar').on('click', '.signin.item', signInAlert);
+$('header.ui.bar').on('click', '.signup.item', signUpAlert);
+$('.more-destination').on('click', '.ui.button', seeMoreDestination);
 
-function search(dest){
-     var dateS=$('.search-bar .from').find('input').val();
-     var dateE=$('.search-bar .to').find('input').val();
-     var passenger=$('.search-bar .passengers').find('input').val();
-     if(!dest&&!dateS&&!dateE&&!passenger){
-       alert('please choose one option for search at least ~');
-     }
-     else{
-       location.href='/search?destination='+dest+'&date_start='+dateS+'&data_end='+dateE+'&passenger='+passenger;
-     }
+function search(dest) {
+    var dateS = $('.search-bar .from').find('input').val();
+    var dateE = $('.search-bar .to').find('input').val();
+    var passenger = $('.search-bar .passengers').find('input').val();
+    if (!dest && !dateS && !dateE && !passenger) {
+        alert('please choose one option for search at least ~');
+    } else {
+        location.href = '/search?destination=' + dest + '&date_start=' + dateS + '&data_end=' + dateE + '&passenger=' + passenger;
+    }
 }
-function seeMoreDestination(){
-    var $this=$(this);
+
+function seeMoreDestination() {
+    var $this = $(this);
     $.ajax({
-        url:'/popular/more',
-        type:'GET',
-        data:{},
-        success:function(r){
-            var tpl=''
-            r.forEach((v,k)=>{
-                tpl+=`<a class="card" href="${v.url?v.url:'#'}">
+        url: '/popular/more',
+        type: 'GET',
+        data: {},
+        success: function (r) {
+            var tpl = ''
+            r.forEach((v, k) => {
+                tpl += `<a class="card" href="${v.url?v.url:'#'}">
                     <div class="ui  image">
                       <div class="ui dimmer visible active">
                         <div class="content">
@@ -139,75 +143,93 @@ function seeMoreDestination(){
             $('.third-sec .cards').append(tpl);
             $this.remove();
         },
-        error:function(err){
-              
+        error: function (err) {
+
         }
     })
 }
 
-function signIn(){
+function signIn() {
     // $('header.ui.bar  .right.menu').addClass('signed');
     // return;
-    var email=$('#signin-email-input').val(),
-        password=$('#signin-password-input').val();
-        console.log(email,password)
-        if(email&&password){
-            $.get('/user/login/',{email:email,password:password},function(r){
-                console.log(r);
-                if(r.message=="success"){
-                    $('header.ui.bar  .right.menu').addClass('signed');
-                    $('header.ui.bar  .right.menu').text(r.user)
-                    signInDialog.modal('hide');  
-                }else{
-                    alert('sorry , someting wrong ')
-                }
-
-            })
-        }else{
-            alert('Please fill in all options ~');
-        }
-    
-}
-function signUp(){
-    var email=$('#signup-email-input').val(),
-        password=$('#signup-password-input').val(),
-        name=$('#signup-name-input').val(),
-        password_confirmation=$('#signup-confirm-password-input').val();
-    if(email&&password&&name&&password_confirmation){
-        $.get('/user/register',
-          {email:email,
-            password:password,
-            password_confirmation:password_confirmation,
-            name:name
-          },
-          function(r){
-                    console.log(r);
-                    alert('registration succeeded!');
-                    signInAlert();
-          })
-    }else{
-      alert('Please fill in all options ~');
-    }
-   
-}
-function signOut(){
-  //  $('header.ui.bar  .right.menu').removeClass('signed'); 
-  // return;
-   $.get('/user/logout',function(r){
+    var email = $('#signin-email-input').val(),
+        password = $('#signin-password-input').val();
+    console.log(email, password)
+    if (email && password) {
+        $.get('/user/login/', {
+            email: email,
+            password: password
+        }, function (r) {
             console.log(r);
-            $('header.ui.bar  .right.menu').removeClass('signed'); 
-            alert('logout succeeded')   
-      })
+            if (r.message == "success") {
+                $('header.ui.bar  .right.menu').addClass('signed');
+                $('header.ui.bar  .right.menu').text(r.user)
+                signInDialog.modal('hide');
+            } else {
+                alert('sorry , someting  went wrong ')
+            }
+
+        })
+    } else {
+        alert('Please fill in all options ~');
+    }
+
 }
-function signInAlert(){  
-  signInDialog.modal({
-        blurring:true,
-        transition:'fade up'
+
+function signUp() {
+    var email = $('#signup-email-input').val(),
+        password = $('#signup-password-input').val(),
+        name = $('#signup-name-input').val(),
+        password_confirmation = $('#signup-confirm-password-input').val();
+    if (email && password && name && password_confirmation) {
+        $.ajax({
+            url: '/user/register',
+            type: 'GET',
+            data: {
+                email: email,
+                password: password,
+                password_confirmation: password_confirmation,
+                name: name
+            },
+            success: function (r) {
+                console.log(r);
+                if (r.code == 0) {
+                    signInAlert();
+                } else {
+                    alert(r.message);
+                }
+            },
+            error: function (err) {
+                alert()
+            }
+        })
+
+    } else {
+        alert('Please fill in all options ~');
+    }
+
+}
+
+function signOut() {
+    //  $('header.ui.bar  .right.menu').removeClass('signed'); 
+    // return;
+    $.get('/user/logout', function (r) {
+        console.log(r);
+        $('header.ui.bar  .right.menu').removeClass('signed');
+        alert('logout succeeded')
+    })
+}
+
+function signInAlert() {
+    signInDialog.modal({
+        blurring: true,
+        transition: 'fade up'
     }).modal('show');
 }
-function signUpAlert(){
-  signUpDialog.modal({
-        blurring:true,
-        transition:'fade up'
+
+function signUpAlert() {
+    signUpDialog.modal({
+        blurring: true,
+        transition: 'fade up'
     }).modal('show');
 }
